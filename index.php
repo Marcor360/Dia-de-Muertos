a5864637544f6a2a0bf29f2ccaf792e3cfb4670c
Marco Antonio Rulfo Castro <81320236+Marcor360@users.noreply.github.com>
2021-10-28 14:42:36 -0500

    Creacion de Repositorio
    Se crea el repositorio

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Inicio | México Oculto </title>
+    <link rel="stylesheet" href="CSS\bootstrap.min.css"><!--- Enlace de Boostrap.CSS --->
+  </head>
+  <!--- Empieza navbar --->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">México Oculto</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Hogar</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Mictlán_Que_es.php">¿Que es el Mictlán?</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">¿Como llegamos al Mictlán?</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="Dioses_Tipos" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dioses
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="Dioses_Tipos">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  <!--- Finaliza navbar --->
+  <!--- Empieza Slider --->
+
+  <!--- Finaliza Slider --->
+  <body>
+    <!--- Empieza Pie de Paguina --->
+    <footer>
+      <div class="text-center py-2">
+        Derechos reservados | México Oculto
+      </div>
+      <div class="text-center">
+        Contacto | 5532083451
+      </div>
+      <div class="text-center">
+        Email | Méxicooculto@contacto.com.mx
+      </div>
+    </footer>
+    <!--- Finaliza el pie de Paguina --->
+<!--- Empieza escript ---> <script src="JS\bootstrap.min.js"></script><!--- Finaliza script --->
+  </body>
+</html>
